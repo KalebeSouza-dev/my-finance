5e1afe01a2ee1405c61d43297e88eb29c0a3e2b4
KalebeSouza-dev <kalebesouza.cc@gmail.com>
2026-08-25 14:55:51 -0300

    feat: create front calculator app

--- README.md
+++ README.md
@@ -11,3 +11,14 @@ Este projeto foi construído utilizando:
 * **Python 3**
 * **SQLite**: Banco de dados local
 * **CustomTkinter**: Interface Gráfica Python.
+
+---
+
+Para buildar o app localmente, execute os seguintes comandos em um ambiente python:
+
+```bash
+    pip install pyinstaller
+    pip install -r requirements.txt
+        
+    pyinstaller --noconsole --onefile my-finance.py
+```
--- views/calculator_app.py
+++ views/calculator_app.py
@@ -7,15 +7,61 @@ class CalculatorAPP:
 
         # window
         root.title("Calculadora de Juros") 
-        root.geometry("300x230")
+        root.geometry("300x400")
         root.resizable(False, False)
 
-        # valor inicial
-        # taxa anual
-        # aporte mensal
-        # tempo
+        # inputs
+        self.entry_valor_inicial = ctk.CTkEntry(root, placeholder_text="Valor Inicial (R$)", width=200)
+        self.entry_valor_inicial.pack(pady=3)
+
+        self.entry_aporte_mensal = ctk.CTkEntry(root, placeholder_text="Aporte Mensal (R$)", width=200)
+        self.entry_aporte_mensal.pack(pady=3)
+
+        self.entry_taxa_anual = ctk.CTkEntry(root, placeholder_text="Taxa Anual (%)", width=200)
+        self.entry_taxa_anual.pack(pady=3)
+
+        self.entry_periodo = ctk.CTkEntry(root, placeholder_text="Tempo (Meses)", width=200)
+        self.entry_periodo.pack(pady=3)
+
+        self.entry_valor_inicial.bind("<Return>",lambda event: self.entry_aporte_mensal.focus()) 
+        self.entry_aporte_mensal.bind("<Return>", lambda event: self.entry_taxa_anual.focus())   
+        self.entry_taxa_anual.bind("<Return>", lambda event: self.entry_periodo.focus()) 
+        self.entry_periodo.bind("<Return>", self.exibir_calculo)
+
+        # button
+        self.btn_calcular = ctk.CTkButton(root, text="Calcular", width=200, command=self.exibir_calculo)
+        self.btn_calcular.pack(pady=7)
+
+
+        # frame result
+        self.result_frame = ctk.CTkFrame(root, fg_color="transparent")
+        self.result_frame.pack(pady=5, fill="x", padx=10)
+
+        self.lbl_investido = ctk.CTkLabel(self.result_frame, text="Investido: R$ 0.00")
+        self.lbl_investido.pack(side="left", expand=True)
+        self.lbl_juros = ctk.CTkLabel(self.result_frame, text="Juros: R$ 0.00")
+        self.lbl_juros.pack(side="right", expand=True)
+
+        self.lbl_final = ctk.CTkLabel(root, text="Valor Final: R$ 0.00", font=("Arial", 16, "bold"))
+        self.lbl_final.pack(pady=10)
+
+    def exibir_calculo(self, event=None):
+        try:
+            valor_inicial = float(self.entry_valor_inicial.get().replace(",", "."))
+            aporte_mensal = float(self.entry_aporte_mensal.get().replace(",", "."))
+            taxa_anual = float(self.entry_taxa_anual.get().replace(",", "."))
+            periodo = int(self.entry_periodo.get())
+
+            resultados = self.calculo(valor_inicial, aporte_mensal, taxa_anual, periodo)
+
+            self.lbl_investido.configure(text=f"Investido: R$ {resultados['valor_investido']:.2f}")
+            self.lbl_juros.configure(text=f"Juros: R$ {resultados['valor_juros']:.2f}")
+            self.lbl_final.configure(text=f"Valor Final: R$ {resultados['valor_final']:.2f}")
+            
+        except ValueError:
+            self.lbl_final.configure(text="Erro: Preencha com números válidos!")
     
-    def calculo(self, valor_inicial, aporte_mensal, juros_anual, periodo) -> dict:
+    def calculo(self,valor_inicial, aporte_mensal, juros_anual, periodo) -> dict:
         juros_mensal = (1 + (juros_anual / 100)) ** (1 / 12) - 1
         juros_por_periodo = (1 + juros_mensal) ** periodo
 
