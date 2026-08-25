2fd5c70d58f55d9f4c31dd67ca6f671ec36c2e9c
KalebeSouza-dev <kalebesouza.cc@gmail.com>
2026-08-24 23:02:48 -0300

    feat: initial version of the interest calculation

--- views/calculator_app.py
+++ views/calculator_app.py
@@ -14,3 +14,20 @@ class CalculatorAPP:
         # taxa anual
         # aporte mensal
         # tempo
+    
+    def calculo(self, valor_inicial, aporte_mensal, juros_anual, periodo) -> dict:
+        juros_mensal = (1 + (juros_anual / 100)) ** (1 / 12) - 1
+        juros_por_periodo = (1 + juros_mensal) ** periodo
+
+        valorMontante = valor_inicial * juros_por_periodo
+        valorAporte = (aporte_mensal / juros_mensal) * (juros_por_periodo - 1)
+
+        valor_final = valorMontante + valorAporte
+        valor_juros = valor_final - (valor_inicial + periodo * aporte_mensal)
+        valor_investido = valor_final - valor_juros
+
+        print(f"{'  Valor Final:':<20} {'R$ ':>5}{valor_final:>10.2f}")
+        print(f"{'  Valor Juros:':<20} {'R$ ':>5}{valor_juros:>10.2f}")
+        print(f"{'  Valor Investido:':<20} {'R$ ':>5}{valor_investido:>10.2f}")
+
+        return {"valor_final": valor_final, "valor_juros": valor_juros, "valor_investido": valor_investido}
